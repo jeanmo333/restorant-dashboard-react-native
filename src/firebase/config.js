a35e4867212b716c606d677e08eaf3ed14addd40
Moril <jeanmo333@gmail.com>
2023-02-21 19:55:01 -0300

    commit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,6 @@
 // AGREGA TUS CREDENCIALES AQUI
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDsOEFjAaQnfFhieKPbmsVcgiYlfeYt60U",
-  authDomain: "restorant-app-46903.firebaseapp.com",
-  projectId: "restorant-app-46903",
-  storageBucket: "restorant-app-46903.appspot.com",
-  messagingSenderId: "128957216201",
-  appId: "1:128957216201:web:5d6198978f37bc6c556418",
-  measurementId: "G-CV98SL0804",
-};
+const firebaseConfig = {};
 
 export default firebaseConfig;
